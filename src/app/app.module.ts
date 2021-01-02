3daf5319e5a2b45ba495eed88df93103ed7b68fd
Nicholas Hazel <nicholas.hazel@gmail.com>
2021-01-02 01:29:36 -0800

    feat: create-character page and scaffolding in place

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameEngine } from './game/game.engine';
@@ -22,6 +22,7 @@ import { CharacterCreateComponent } from './scenes/character-create/character-cr
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule
   ],
   providers: [],
