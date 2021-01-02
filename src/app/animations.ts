a925ee432f83216f0ebbed825e825f7fe5fd0418
Nicholas Hazel <nicholas.hazel@gmail.com>
2021-01-02 02:33:28 -0800

    refactor: restyled router animations and character create

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -12,12 +12,12 @@ export const fadeAnimation = trigger('fadeAnimation', [
     query(':enter, :leave', style({ position: 'fixed', height:'100%', opacity: 1 })),
     group([ 
       query(':enter', [
-        style({ opacity: 0, transform: 'translateY(20%)' }),
-        animate('0.5s 0.5s ease-in-out', style({ opacity: 1, transform: 'translateY(0%)' }))
+        style({ opacity: 0, transform: 'translateX(20%)' }),
+        animate('0.5s 0.5s ease-in-out', style({ opacity: 1, transform: 'translateX(0%)' }))
       ]),
       query(':leave', [
-        style({ opacity: 1, transform: 'translateY(0%)' }),
-        animate('0.5s ease-in-out', style({ opacity: 0, transform: 'translateY(-20%)' }))]),
+        style({ opacity: 1, transform: 'translateX(0%)' }),
+        animate('0.5s ease-in-out', style({ opacity: 0, transform: 'translateX(-20%)' }))]),
     ])
   ])
 ]);
